b28e2d752aa95f2fbc6061f4e130c0fef33877cf
JUNESSY1 <junetogro1102@gmail.com>
2025-08-28 22:14:53 -0600

    Add files via upload

---
+++ ISC_FPC_EVA1_3_DATOS_25550471/src/isc_eva1_3_datos_25550471/ISC_EVA1_3_DATOS_25550471.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package isc_eva1_3_datos_25550471;
+
+/**
+ *
+ * @author equipo
+ */
+public class ISC_EVA1_3_DATOS_25550471 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("NOMBRE DEL ESTUDIANTE: ");
+        System.out.println("JUNESSY TORRES");
+        System.out.print("NÚMERO DE CONTROL: ");
+        System.out.println(25550471);
+        System.out.print("EDAD: ");
+        System.out.println(18);
+    }
+    
+}
---
+++ ISC_FPC_EVA1_4_EMPLEO_25550471/src/isc_eva1_4_empleo_25550471/ISC_EVA1_4_EMPLEO_25550471.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package isc_eva1_4_empleo_25550471;
+
+/**
+ *
+ * @author equipo
+ */
+public class ISC_EVA1_4_EMPLEO_25550471 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("EMPLEO: ");
+        System.out.println("GESTOR DE COBRANZA");
+        System.out.print("SALARIO: ");
+        System.out.println(4500);
+        System.out.print("HORAS A LA SEMANA: ");
+        System.out.println(60);
+        System.out.print("REQUIERE INGLÉS? ");
+        System.out.print("FALSE");
+    }
+    
+}
