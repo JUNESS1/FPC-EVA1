2c18595afc79e2ca62e6f5e6dcd252881c1f978b
JUNESSY1 <junetogro1102@gmail.com>
2025-09-03 00:28:03 -0600

    Add files via upload

---
+++ ISC_FPC_EVA1_7_AREA_TRIANGULO_25550471/src/eva1_7_area_triangulo/EVA1_7_AREA_TRIANGULO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_area_triangulo;
+
+/**
+ *
+ * @author equipo
+ */
+public class EVA1_7_AREA_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //VARIABLES:
+        double base, altura, area;
+        //VALORES DE LAS VARIABLES:
+        base = 15;
+        altura = 20;
+        //OPERACIÓN:
+        area = (base * altura)/2;
+        //SALIDA DE DATOS:
+        System.out.println("El área del triángulo es:");
+        System.out.println(area);
+    }
+    
+}
---
+++ ISC_FPC_EVA1_8_TEMPERATURA_25550471/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author equipo
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //VARIABLES:
+        double C, F;
+        //VALORES DE LAS VARIABLES:
+        F = 50;
+        //OPERACIÓN:
+        C = 5 * (F - 32)/9;
+        //SALIDA DE DATOS:
+        System.out.println("La temperatura de grados Fahrenheit a Celsius es: ");
+        System.out.println(C);
+    }
+    
+}
